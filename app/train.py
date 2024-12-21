abc7ea5f5f49d0b556c6dd864577f7684e43c19c
mert <55oguzhan.mert@gmail.com>
2024-12-21 20:26:08 +0300

    train.py class is added.

---
+++ app/train.py
@@ -0,0 +1,20 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from dataclasses import dataclass
+from transformers import pipeline
+
+
+@dataclass
+class TurkishClassification:
+
+    def __post_init__(self):
+        pass
+
+    def train(self):
+        pass
+
+
+
+
+
+if __name__ == "__main__":
+    pass
